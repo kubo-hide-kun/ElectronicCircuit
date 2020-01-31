44ef35f1bac7ec6451d44c886ebc13130316b0df
kubo-hide-kun <41711771+kubo-hide-kun@users.noreply.github.com>
2020-01-31 10:12:35 +0900

    Update exam2.ino

--- exam2.ino
+++ exam2.ino
@@ -1,40 +1,32 @@
 int roop = 0;
+int i;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A0,INPUT);
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);
+   pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  /*pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
   pinMode(10,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(11,OUTPUT);*/
+  pinMode(8,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(10,HIGH);
-  int pw1 = analogRead(A0);
-  if( roop++ % 500 == 0)Serial.println(pw1);
-  // false: 前進, true: 後退
-  ctrl(pw1 < 330, pw1 < 330);
-}
-
-void ctrl(bool right, bool left) {
-  
-  if(right) {
-    digitalWrite(8,LOW);
-    digitalWrite(9,LOW);
-  } else {
-    digitalWrite(9,HIGH);
-    digitalWrite(8,LOW);;
-  }
-  
-  if(left) {
-    digitalWrite(13,LOW);
-    digitalWrite(12,LOW);
-  } else {
-    digitalWrite(12,HIGH);
-    digitalWrite(13,LOW);
-  }
-  
+  digitalWrite(8,HIGH);
+  int pw1 = analogRead(A0); //pw1:基準150 右側
+  int pw2 = analogRead(A1); //pw2:基準140　左側
+  if( roop++ % 500 == 0)Serial.println(pw2);
+    if(pw1>150){
+      analogWrite(5,100);
+      analogWrite(3,0);
+      analogWrite(10,100);
+      analogWrite(11,0);
+    }else{
+        analogWrite(5,0);
+        analogWrite(3,0);
+        analogWrite(10,0);
+        analogWrite(11,0);
+      }
 }
